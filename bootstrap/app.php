cf43dc784a80fee0bafd7a6ea8335d7242923c80
paresiqbal <thecrowdel@gmail.com>
2025-11-16 01:46:42 +0700

    Add RoleMiddleware and update routes for role-based access control

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -21,7 +21,13 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        // user middleware
+        $middleware->alias([
+            'role' => App\Http\Middleware\RoleMiddleware::class,
+        ]);
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,28 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
+
+// Route Admin
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return inertia('Admin/Dashboard');
+    });
+});
+
+// Route Teacher
+Route::middleware(['auth', 'role:teacher'])->group(function () {
+    Route::get('/teacher/bank', function () {
+        return inertia('Teacher/QuestionBank');
+    });
+});
+
+
+// Route Student
+Route::middleware(['auth', 'role:student'])->group(function () {
+    Route::get('/exam/start', function () {
+        return inertia('Student/StartExam');
+    });
+});
+
+
+require __DIR__ . '/settings.php';
